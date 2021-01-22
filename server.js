d443729dfadf6a66f74443416fb8d29c284f773c
Ihab <Ihab.ah1@gmail.com>
2021-01-22 10:45:43 +0200

    update

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+var path = require('path');
+
+app.use(express.static('public'));
+
+const port = process.env.PORT || 3000;
+app.listen(port,function() {
+console.log('listening',port);
+})
